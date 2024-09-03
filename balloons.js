bb3205c90ec75af1bc7cdda8d88519822ab59dd5
Yose Rafhael <yrafhael@my.wctc.edu>
2024-09-03 08:49:48 -0500

    [1.01] Initial commit - Lab activity

---
+++ balloons.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const elem = document.getElementById('dob');
+    const datepicker = new Datepicker(elem, {
+        // options
+        autohide: true,
+        format: 'MM-dd'
+      });
+
+  // uncheck all boxes by default (Firefox)
+  document.querySelectorAll('.form-check-input').forEach(c => c.checked = false);
+
+  // event listener for check/uncheck
+  document.getElementById('checkbox-card').addEventListener('change', function(e){
+    if (e.target.classList.contains('form-check-input')) {
+      const elem = document.getElementById(e.target.id + 'Img');
+      elem.style.visibility = "visible";
+      elem.classList.remove("animate__animated", "animate__bounceInDown", "animate__bounceOutUp");
+      e.target.checked ?
+        elem.classList.add("animate__animated", "animate__bounceInDown") :
+        elem.classList.add("animate__animated", "animate__bounceOutUp");
+    }
+  });
+});
